535a3d5689a82459073a7dcf8719a0c344b0ad85
Ojonuba Jeremiah <ojonuba93@gmail.com>
2022-11-02 17:00:05 +0100

    created the database Migrations modules

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from config import Config
+
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
