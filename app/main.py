39c8e2054e77c28ebd43918183af4026e3ad0671
Ojonuba Jeremiah <ojonuba93@gmail.com>
2022-11-02 17:03:19 +0100

    entry point to the app

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from app import app
+from app import db
+import views
+from posts.blueprint import posts
+
+app.register_blueprint(posts, url_prefix='/blog')
+
+
+if __name__ == "__main__":
+    app.run()
