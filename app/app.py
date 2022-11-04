02f0844f4e6af12f4f4aabfd1112edecbc0d18c6
Ojonuba Jeremiah <ojonuba93@gmail.com>
2022-11-04 07:13:07 +0100

    Updated codes with custom css, custom 404 error page and updated models

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_migrate import Migrate, MigrateCommand
@@ -6,9 +7,11 @@ from flask_script import Manager
 
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_admin import AdminIndexView
 
 from flask_security import SQLAlchemyUserDatastore
 from flask_security import Security
+from flask_security import current_user
 
 from config import Config
 
@@ -25,10 +28,41 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+class AdminMixin:
+    def is_accessible(self):
+        return current_user.has_role('admin')
 
-admin = Admin(app)
-admin.add_view(ModelView(Post, db.session))
-admin.add_view(ModelView(Tag, db.session))
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('security.login',
+        next=request.url))
+
+
+class AdminView(AdminMixin, ModelView):
+    pass
+
+
+class HomeAdminView(AdminMixin, AdminIndexView):
+    pass
+
+class BAseModelView(ModelView):
+    def on_model_change(self, form, model, is_created):
+        if is_created:
+            model.generate_slug()
+        return super().on_model_change(form, model,
+         is_created)
+class PostAdminView(AdminMixin, BAseModelView):
+    form_columns = ['title', 'body', 'tags']
+
+class TagAdminView(AdminMixin, BAseModelView):
+    pass
+
+
+admin = Admin(app, 'FlaskApp', url='/',
+index_view=HomeAdminView(name='Home'))
+
+
+admin.add_view(PostAdminView(Post, db.session))
+admin.add_view(TagAdminView(Tag, db.session))
 
 #Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
--- app/views.py
+++ app/views.py
@@ -7,3 +7,8 @@ from app import app
 def index():
     animal = 'Tiger'
     return render_template('index.html', name=animal)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
