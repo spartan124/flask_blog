8cc37c15bbc97b43162f399f0d5e51239529514e
Ojonuba Jeremiah <ojonuba93@gmail.com>
2022-11-04 13:50:00 +0100

    modified the app.route content to display posts_list as homepage

--- app/views.py
+++ app/views.py
@@ -1,12 +1,31 @@
 from flask import render_template
+from flask import request
+from posts.blueprint import *
 
 from app import app
 
 
 @app.route('/')
 def index():
-    animal = 'Tiger'
-    return render_template('index.html', name=animal)
+    q = request.args.get('q')
+
+    if q:
+        posts = Post.query.filter(Post.title.contains(q) |
+        Post.body.contains(q))
+    else:
+        posts = Post.query.order_by(Post.created.desc())
+    
+    page = request.args.get('page')
+
+    if page and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
+    pages = posts.paginate(page=page, per_page=1)
+
+    return render_template('posts/posts.html', posts=posts,
+    pages=pages)
 
 
 @app.errorhandler(404)
