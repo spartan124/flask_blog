9bac467bc3ff05028b0077f8d2f1459c165cb774
Ojonuba Jeremiah <ojonuba93@gmail.com>
2022-11-08 15:04:39 +0100

    improved the codes for the app routes, register and login user

--- app/views.py
+++ app/views.py
@@ -1,12 +1,11 @@
-from flask import render_template
-from flask import request, url_for, redirect
+from flask import render_template, request, url_for, redirect, flash
 from posts.blueprint import *
 from posts.forms import *
 
 from app import *
-from flask_bcrypt import Bcrypt
 
-bcrypt = Bcrypt(app)
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 @app.route('/')
 def index():
@@ -36,30 +35,50 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user)
-            return redirect(url_for('posts.posts_list'))
-            if current_user.is_authenticated:
+        if user:
+            if check_password_hash(user.password_hash, form.password.data):
+                login_user(user)
+                flash("Login Successfull!!!")
                 return redirect(url_for('posts.posts_list'))
+            else:
+                flash("Wrong Password - Try Again...")
+        else:
+            flash("User Doesn't Exist! Try Again...")
+
+
+                # return redirect(url_for('posts.posts_list'))
     return render_template('login.html', form=form)
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     logout_user()
-    return redirect_url(url_for('login'))
+    flash("You Have Been Logged Out! Thanks For Dropping By....")
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET','POST'])
 def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(firstname=form.firstname.data, lastname=form.lastname.data,
-        username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None:
+            #Hash the password
+            hashed_password = generate_password_hash(form.password_hash.data, "sha256")
+            new_user = User(firstname=form.firstname.data, lastname=form.lastname.data,
+            username=form.username.data, email=form.email.data, password_hash=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
 
+        firstname = form.firstname.data
+        form.firstname.data = ''
+        form.lastname.data = ''
+        form.username.data = ''
+        form.email.data = ''
+        form.password_hash.data = ''
+
+        flash("User Added Successfully!")
+
+    our_users = User.query.order_by(User.date_added)
     return render_template('register.html', form=form)
 
 
