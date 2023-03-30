3ce3ebd1e622dd0e5cfa0fc60a008fb9c443b694
Jeremiah Ojonuba <ojonuba93@gmail.com>
2023-03-30 10:32:01 +0100

    remodelling admin views

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from auth import (ContactAdminView, HomeAdminView, PostAdminView,
+                  RoleAdminView, TagAdminView, UserAdminView)
 from config.config import config_dict
-from utils import db
-from flask_migrate import Migrate
+from flask import Flask
+from flask_admin import Admin
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from models import ContactUs, Post, Role, Tag, User
+from utils import db
+
 
 def create_app(config=config_dict['dev']):
     app = Flask(__name__)
@@ -17,7 +22,20 @@ def create_app(config=config_dict['dev']):
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id))
+        return User.query.get_or_404(int(user_id))
+    
+    admin = Admin(app, 'Blogify', url='/',
+    index_view=HomeAdminView(name='Home'))
+
+
+    admin.add_view(PostAdminView(Post, db.session))
+    admin.add_view(TagAdminView(Tag, db.session))
+    admin.add_view(ContactAdminView(ContactUs, db.session))
+    admin.add_view(UserAdminView(User, db.session))
+    admin.add_view(RoleAdminView(Role, db.session))
+
+    return app
+
 
     
     
